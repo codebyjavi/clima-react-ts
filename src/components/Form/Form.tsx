631530a273e4f6c6b1500d382cb3228bf791a037
javierspr <learn.javier@gmail.com>
2024-07-25 13:37:09 -0300

    initial commit

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,39 @@
+import { countries } from "../../data/countries";
+import styles from './Form.module.css'
+
+export default function Form() {
+  return (
+    <form className={styles.form}>
+
+        <div className={styles.field}>
+            <label htmlFor="city">Ciudad:</label>
+            <input 
+                type="text"
+                id="city" 
+                name="city"
+                placeholder="Ciudad"
+            />
+        </div>
+
+        <div className={styles.field}>
+            <label htmlFor="country">País:</label>
+            <select 
+                name="country" 
+                id="country"
+            >
+                <option value="">-- Seleccione un País --</option>
+                {countries.map( country => (
+                    <option 
+                        value={country.code}
+                        key={country.code}
+                    >
+                        {country.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+
+        <input className={styles.submit} type="submit" value='Consultar Clima' />
+    </form>
+  )
+}
